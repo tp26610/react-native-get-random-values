b9d8c791c8e7b38bbf94001826044d11b94323ca
Brian Yeh <brian.yeh@coolbitx.com>
2025-10-19 11:46:09 +0800

    feat: update example to test getRandomBase64 and getRandomValues

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,18 +1,28 @@
 import { useState } from 'react';
-import { Text, View, StyleSheet, Button } from 'react-native';
+import { Text, View, Button, StyleSheet } from 'react-native';
 import { getRandomBase64 } from 'react-native-get-random-values';
 
 export default function App() {
-  const [randomValue, setRandomValue] = useState('');
+  const [randomBase64, setRandomBase64] = useState('');
+  const [randomValuesStr, setRandomValuesStr] = useState('');
   return (
     <View style={styles.container}>
       <Button
-        title="Generate Random Value"
+        title="get random base64 (10 bytes)"
         onPress={() => {
-          setRandomValue(getRandomBase64(10));
+          setRandomBase64(getRandomBase64(10));
         }}
       />
-      <Text>Result: {randomValue}</Text>
+      <Text>GetRandomBase64 Result: {randomBase64}</Text>
+      <Button
+        title="get random values (10 length array)"
+        onPress={() => {
+          const getRandomValues = (global.crypto as any).getRandomValues;
+          const randomValues = getRandomValues(new Uint8Array(5));
+          setRandomValuesStr(randomValues.join(','));
+        }}
+      />
+      <Text>GetRandomValues Result: {randomValuesStr}</Text>
     </View>
   );
 }
@@ -24,5 +34,6 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     gap: 16,
     backgroundColor: 'white',
+    padding: 32,
   },
 });
